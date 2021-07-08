0499cb10bf501f43b2f8262e56c842fdb6c0ef0b
felix <you@example.com>
2021-07-08 15:08:20 +0300

    File Modified

--- PRACTICE3/programmiz2/src/Form/label.js
+++ PRACTICE3/programmiz2/src/Form/label.js
@@ -2,36 +2,39 @@ import React, { Component } from "react";
 class Label extends Component {
   render() {
     return (
-      <div>
-        Enter Full Name:
-        <input type="text" placeholder="Enter full name" />
-        <br></br>
-        Enter Username :
-        <input type="text" placeholder="Enter username" />
-        <br></br>
-        Enter Email:
-        <input type="email" placeholder="Enter email" />
-        <br></br>
-        Enter Password :
-        <input type="password" placeholder="Enter password" />
-        <br></br>
-        Confirm Password:
-        <input type="password2" placeholder="Confirm password" />
-        <br></br>
-        <div>
-          Gender
-          <input type="radio" value="Male" name="gender" />
-          Male
-          <input type="radio" value="Female" name="gender" />
-          Female
-          <input type="radio" value="Other" name="gender" />
-          Other
-        </div>
-        93+172 :
-        <input type="text" placeholder="Enter result" />
-        <br></br>
-        <input type="checkbox" />
-        Agree with the terms and conditions
+      <div className="container">
+        <form>
+          <h1>Register With Us</h1>
+          <label className="container-label">Enter Full Name:</label>
+          <input type="text" placeholder="Enter full name" />
+          <br></br>
+          <label className="container-label">Enter Username :</label>
+          <input type="text" placeholder="Enter username" />
+          <br></br>
+          <label className="container-label">Enter Email:</label>
+          <input type="email" placeholder="Enter email" />
+          <br></br>
+          <label className="container-label">Enter Password :</label>
+          <input type="password" placeholder="Enter password" />
+          <br></br>
+          <label className="container-label">Confirm Password:</label>
+          <input type="password2" placeholder="Confirm password" />
+          <br></br>
+          <div>
+            <label className="container-radio1">Gender</label>
+            <input type="radio" value="Male" name="gender" />
+            <label className="container-radio2">Male</label>
+            <input type="radio" value="Female" name="gender" />
+            <label className="container-radio2">Female</label>
+            <input type="radio" value="Other" name="gender" />
+            <label className="container-radio3">Other</label>
+          </div>
+          <label className="container-checkbox">93+172 :</label>
+          <input type="text" placeholder="Enter result" />
+          <br></br>
+          <input type="checkbox" />
+          <label>Agree with the terms and conditions</label>
+        </form>
       </div>
     );
   }
